53affeaff6b52689f38057a888cc642ba9c78b59
Dev-Jogani <devmjogani@gmail.com>
2025-03-18 20:45:39 +0530

    coloring

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-export const App =()=>{
-  return <h1>Hello Developer Kushal, Welcome to hustle of Coding with Dev Jogani.</h1>;
+export const App = () => {
+  return (
+    <div>
+      <button style={{marginRight:"150px", fontSize: "50px",height:"400px",width:"600px", padding: "20px 40px",background: "linear-gradient(to right,rgb(255, 159, 95),rgb(229, 255, 0))"}}>
+        Dev's Work...
+      </button>
+      <button style={{ fontSize: "50px",height:"400px",width:"600px", padding: "20px 40px", background: "linear-gradient(to right, #ff7e5f,rgb(23, 249, 245))", }}>
+        Kushal's Work...
+      </button>
+      <h1>
+        Hello Developer Kushal, Welcome to the hustle of Coding with Dev Jogani.
+      </h1>
+    </div>
+  );
 };
