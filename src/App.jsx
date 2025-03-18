02fcb6ff2cd0d7635a5d83c8342df4c2b3e85d09
Dev-Jogani <devmjogani@gmail.com>
2025-03-18 16:47:43 +0530

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export const App =()=>{
-  return <h1>Hello Developer Kushal, Welcome to hustle of Coding.</h1>;
+  return <h1>Hello Developer Kushal, Welcome to hustle of Coding with Dev Jogani.</h1>;
 };
