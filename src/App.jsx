c8dcdf04d9283ca46a1dec656ea7f193f5ee8a7b
Dev-Jogani <devmjogani@gmail.com>
2025-03-18 22:26:58 +0530

    mycard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 export const App = () => {
   return (
     <div>
-      <button style={{marginRight:"150px", fontSize: "50px",height:"400px",width:"600px", padding: "20px 40px",background: "linear-gradient(to right,rgb(255, 159, 95),rgb(229, 255, 0))"}}>
-        Dev's Work...
-      </button>
+     <div className="ProfileCard">
+      <img className="profile-img" src="profile.jpg" alt="profile.jpg" width={"25%"}></img>
+      <h2>Name:Dev Jogani</h2>
+      <h3>Age:21</h3>
+      <h3>Relationship: Single</h3>
+     </div>
       <h1>
         Hello Developer Kushal, Welcome to the hustle of Coding with Dev Jogani.
       </h1>
