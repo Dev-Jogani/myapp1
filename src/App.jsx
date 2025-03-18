bf308d94069b173dfaa23ce426c978a70a90b171
Dev-Jogani <devmjogani@gmail.com>
2025-03-18 23:13:47 +0530

    other card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+import React from "react";
+
 export const App = () => {
   return (
-    <div>
-     <div className="ProfileCard">
-      <img className="profile-img" src="profile.jpg" alt="profile.jpg" width={"25%"}></img>
-      <h2>Name:Dev Jogani</h2>
-      <h3>Age:21</h3>
-      <h3>Relationship: Single</h3>
-     </div>
+    <>
+    <div className="ProfileContainer">
+      <NameCard 
+        name="Dev Jogani" 
+        age={21} 
+        status="Single" 
+        imgSrc="dev.jpg" 
+      />
+      <NameCard 
+        name="Kushal Rajani" 
+        age={22} 
+        status="Committed" 
+        imgSrc="kushal.jpg" 
+      />
+      <NameCard 
+        name="Jigar Jogani" 
+        age={23} 
+        status="Married" 
+        imgSrc="jigar.jpg" 
+      />
+    </div>
       <h1>
         Hello Developer Kushal, Welcome to the hustle of Coding with Dev Jogani.
       </h1>
+      </>
+  );
+};
+
+const NameCard = ({ name, age, status, imgSrc }) => {
+  return (
+    <div className="ProfileCard">
+      <img className="profile-img" src={imgSrc} alt={name} width={"25%"} />
+      <h2>Name: {name}</h2>
+      <h3>Age: {age}</h3>
+      <h3>Relationship: {status}</h3>
     </div>
   );
 };
